9cffcf51f343f26b6504121a5ba2f5d1c4104ec7
Tarun <tarun.rana1999@gmail.com>
2023-06-17 18:42:42 +0530

    Assignment Done

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -6,24 +6,69 @@ public class BankAccount {
     private double balance;
     private double minBalance;
 
-    public BankAccount(String name, double balance, double minBalance) {
+    public BankAccount(String name,double balance,double minBalance) {
+        this.name=name;
+        this.balance=balance;
+        this.minBalance=minBalance;
 
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getMinBalance() {
+        return minBalance;
+    }
+
+    public void setMinBalance(double minBalance) {
+        this.minBalance = minBalance;
     }
 
     public String generateAccountNumber(int digits, int sum) throws Exception{
-        //Each digit of an account number can lie between 0 and 9 (both inclusive)
-        //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
-        //If it is not possible, throw "Account Number can not be generated" exception
+        StringBuilder sb= new StringBuilder();
+       if(digits*9>sum){
+           throw new Exception("Account Number can not be generated");
+        }
+       else {
+           int remainingSum = sum;
+           for (int i = 0; i < digits - 1; i++) {
+               if (remainingSum >= 9) {
+                   sb.append(i);
+                   remainingSum -= i;
+               } else {
+                   sb.append(i);
+                   remainingSum = 0;
+               }
+           }
+       }
+           return sb.toString();
 
-        return null;
-    }
+       }
 
     public void deposit(double amount) {
-        //add amount to balance
-
+        this.balance+=amount;
     }
 
     public void withdraw(double amount) throws Exception {
+        if(this.balance-amount<this.balance){
+            throw new Exception("Insufficient Balance");
+        }
+        else{
+            this.balance-=amount;
+        }
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
 
     }
--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -4,7 +4,18 @@ public class SavingsAccount extends BankAccount{
     double rate;
     double maxWithdrawalLimit;
 
+    public double getRate() {
+        return rate;
+    }
+
+    public double getMaxWithdrawalLimit() {
+        return maxWithdrawalLimit;
+    }
+
     public SavingsAccount(String name, double balance, double maxWithdrawalLimit, double rate) {
+        super(name,balance,0.0);
+        this.maxWithdrawalLimit=maxWithdrawalLimit;
+        this.rate=rate;
         // minimum balance is 0 by default
 
     }
@@ -12,17 +23,22 @@ public class SavingsAccount extends BankAccount{
         // Might throw the following errors:
         // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
         // 2. "Insufficient Balance" : If the amount exceeds balance
+        if(amount>maxWithdrawalLimit){
+            throw new Exception("Maximum Withdraw Limit Exceed");
+        }
+        if(amount>getBalance()){
+            throw new Exception("Insufficient Balance");
+        }
 
     }
 
     public double getSimpleInterest(int years){
-        // Return the final amount considering that bank gives simple interest on current amount
-
+       return((getBalance()*(double)this.rate*(double)years)/100);
     }
 
     public double getCompoundInterest(int times, int years){
         // Return the final amount considering that bank gives compound interest on current amount given times per year
-
+ return this.getBalance() * Math.pow(1.0 + this.rate / (double)(100 * times), (double)(times * years));
     }
 
 }
