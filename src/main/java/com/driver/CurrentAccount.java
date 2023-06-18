e104ecec7da048667fc6518ed2496b1dffb673f0
Tarun <tarun.rana1999@gmail.com>
2023-06-19 03:27:59 +0530

    Bank Application Assignment

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -13,27 +13,69 @@ public class CurrentAccount extends BankAccount {
     }
 
     public void validateLicenseId() throws Exception {
-        char ch[] = tradeLicenseId.toCharArray();
-        int n = tradeLicenseId.length();
-        for (int i = 1; i < n; i++) {
-            if (ch[i] == ch[i - 1]) {
-                for (int j = i + 1; j < n - 1; j++) {
-                    if (ch[j] != ch[i]) {
-                        char temp = ch[i];
-                        ch[i] = ch[j];
-                        ch[j] = temp;
-                        break;
-                    }
-                }
-                // after rearranging it
-                if (ch[i] == ch[i - 1]) {
-                    throw new Exception("Valid License can not be generated");
-                }
+
+        if (!validateId(this.tradeLicenseId)) {
+
+            String rearrangeID = rearrangeTradeID(this.tradeLicenseId);
+            if (rearrangeID == "") {
+
+                throw new Exception("Valid License can not be generated");
+            }
+            this.tradeLicenseId = rearrangeID;
+        }
+    }
+    public String rearrangeTradeID(String s){
+        int n=s.length();
+        //char array for storing the values of all the char
+        char ch[]=s.toCharArray();
+        //freq array to store their freq
+        int freq[]= new int[26];
+
+        //storing the count of each character int freq array
+        for(int i=0;i<26;i++){
+            freq[ch[i]-'a']++;
+        }
+        //find the max count character and its index
+        int max=0;
+        int letter=0;
+        for(int i=0;i<26;i++){
+            if(freq[i]>max){
+                max=freq[i];
+                letter=i;
             }
         }
-        ch.toString();
+        //after placing all the max char,check if char freq is less than half of size than the distinct characters are possible
+        if(max>(n+1)/2){
+            return "";
+        }
+        //placing the highest freq character in ans at even index
+        int idx=0;
+        char ans[]=new char[n];
+        while(freq[letter]>0){
+            ans[idx]= (char) freq[letter+'a'];
+            idx+=2;
+            freq[letter]--;
+        }
 
+        //now placing other charcters in ans
+        for(int i=0;i<26;i++){
+            while(freq[i]>0){
+                if(idx>=n) idx=1;
+                ans[i]= (char) freq[i+'a'];
+                idx+=2;
+                freq[i]--;
+            }
+        }
+        return String.valueOf(ans);
     }
+    public boolean validateId(String s){
+        for(int i=1;i<s.length();i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                return false;
+            }
+        }  return true;
+    }
+
 
     public String getTradeLicenseId() {
         return tradeLicenseId;
